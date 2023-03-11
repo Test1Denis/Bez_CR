a5eaecd67e8a3b2fb51d876fe5087ceea25d907a
Andrey <slawyan.pp@gmail.com>
2023-03-12 01:10:09 +0300

    control work for 10.03.23

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <vector> 
+
+template<typename T>
+bool is_palindrome(T number) {
+	std::vector<T> v;
+// ------------------------
+// split number and push elements to vector     
+        while(number != 0) {
+		T digits = number % 10;
+              	v.push_back(digits);
+               	number /= 10;
+       	}
+// ------------------------
+// palindrome - true or false
+       	for(int i = 0; i < v.size(); ++i) { 
+    		if(v[i] != v[v.size() - 1 - i]) { return false; }
+        }
+	return true;
+}
+
+int main(int argc, char** argv) {
+	FILE* f;
+        int number;
+	int count_of_tests = 0; // count of tests nums in file
+	f = fopen(argv[1], "r");
+	for(int i = 0; i < count_of_tests; i++) {
+		fscanf(f, "%d ", &number);
+		std::cout << is_palindrome(number) << ' ';
+	}
+	std::cout << std::endl;
+        return 0;
+}
